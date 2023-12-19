8dfc68c53e8df704af89c81b9fbc3495be05d2a4
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2023-12-18 20:02:43 -0800

    Add the first segment

--- Modules/1_SnakeGame/Assets/Snake.cs
+++ Modules/1_SnakeGame/Assets/Snake.cs
@@ -7,13 +7,18 @@ public class Snake : MonoBehaviour
     public float UnitsPerSecond = 1.0f;
     public GameObject Head;
 
+    public GameObject SegmentPrefab;
+    public List<GameObject> Segments;
+
     private float LastMoveTime = 0.0f;
     private Vector3 Direction = Vector3.up;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        GameObject firstSegment = Instantiate(SegmentPrefab);
+        Segments.Add(firstSegment);
     }
 
     // Update is called once per frame
